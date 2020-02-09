c81c68d5bebe9766c4a254ffa8162af99d6d0584
suhyunpark1219 <moliling@snu.ac.kr>
2020-02-10 01:10:04 +0900

    Updates

---
+++ precache-manifest.0b9e836b20e772c8e8de23ee1145b0dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762bf1ca6abbe9724ff9001ddcb234da",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e3c904c02108a0de5655",
+    "url": "/movie_app_2020/static/css/main.6d8afb4e.chunk.css"
+  },
+  {
+    "revision": "9d36b778887b3786cbbe",
+    "url": "/movie_app_2020/static/js/2.47af2bfb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.47af2bfb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3c904c02108a0de5655",
+    "url": "/movie_app_2020/static/js/main.19cb67c7.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
